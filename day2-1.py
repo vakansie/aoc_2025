8782619d19f15211ad8f1de7af182949e269719c
vakansie <91331489+vakansie@users.noreply.github.com>
2025-12-02 18:31:29 +0100

    Add files via upload

--- day2-1.py
+++ day2-1.py
@@ -1,15 +1,39 @@
-file = 'day2ex.txt'
+file = 'day2.txt'
 
 def parse_input(file):
     ranges = []
     with open(file, 'r') as file:
         for line in file:
-            print(line.strip())
+            # print(line.strip())
             parts = line.strip().split(',')
-            # ranges.extend(line.strip('\n').split(','))
             ranges.extend([tuple(map(int, part.split('-'))) for part in parts if part])
+    return ranges
 
-    print(ranges)
+def is_repeating(digits:int):
+    string = str(digits)
+    length = len(string)
+    if length % 2:
+        return False
 
+    index = int(length / 2)
+    first, last = string[:index], string[index:]
+    # print(first, last)
+    if first == last:
+        return True
 
-parse_input(file)
+    return False
+
+sum_of = 0
+ranges = parse_input(file)
+print(ranges)
+for rang in ranges:
+    start, end = rang
+    for digits in range(start, end + 1):
+        if is_repeating(digits):
+            print(digits)
+            print('+1')
+            sum_of += digits
+
+print(f'sum = {sum_of}')
+
+# print(is_repeating(123123))
